fedabef2b1b116d4387d93ddcea4a6231f952b47
Asif <asifsaheer7034@gmail.com>
2023-01-12 18:13:23 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,29 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type,auth-token"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.use(session({ secret: "key", cookie: { maxAge: 6000000 } }));
 app.use("/api/admin", adminRoutes);
 const PORT = process.env.PORT || 8000;
