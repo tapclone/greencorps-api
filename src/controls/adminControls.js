f9d24b3335934461753c52aa1fbc0d6e8361ba9e
Asif <asifsaheer7034@gmail.com>
2023-01-13 17:08:53 +0530

    updated

--- src/controls/adminControls.js
+++ src/controls/adminControls.js
@@ -3,7 +3,7 @@ const db = require("../config/db");
 const collection = require("../config/collection");
 const { ObjectId } = require("mongodb");
 const { GALLERY_COLLECTION } = require("../config/collection");
-const {CLIENTS_COLLECTION} = require ('../config/collection')
+const { CLIENTS_COLLECTION } = require("../config/collection");
 const jwt = require("jsonwebtoken");
 const generateToken = (id) => {
   return jwt.sign({ id }, "asif", {
@@ -11,77 +11,91 @@ const generateToken = (id) => {
   });
 };
 const Login = asyncHandler(async (req, res) => {
-
   const { username, password } = req.body;
   if (username == "green_corps" && password == "1234567890") {
     const token = generateToken(password);
 
-    res.status(200).json(token);   
+    res.status(200).json(token);
   } else {
     res.status(401).json("Invalid Details");
   }
 });
 
-
-
-
-
-
 const AddGallery = asyncHandler((req, res) => {
-
-  const obj = req.body
-  const add = db.get().collection(GALLERY_COLLECTION).insertOne(obj)
+  const obj = req.body;
+  const add = db.get().collection(GALLERY_COLLECTION).insertOne(obj);
   if (add) {
-    res.status(200).json("Success")   
+    res.status(200).json("Success");
   } else {
-    res.status(500).json("Somthing Went Wrong")
-  }     
-})
-const viewAllGallery = asyncHandler(async (req, res) => {
-  const AllGallery = await db.get().collection(GALLERY_COLLECTION).find().toArray()
-  if (AllGallery) {
-    res.status(200).json(AllGallery)
-  } else {
-    res.status(201).json("Gallery Empty")
+    res.status(500).json("Somthing Went Wrong");
   }
-})
-const DeleteGallery=asyncHandler(async(req,res)=>{
-  const id=req.params.id
-  const deleteGallery=await db.get().collection(GALLERY_COLLECTION).deleteOne({_id:ObjectId(id)})
- if(deleteGallery){
-  res.status(200).json(deleteGallery)
- }else{
-  res.status(500).json("Something Went Wrong ")
- }
-})
+});
+const viewAllGallery = asyncHandler(async (req, res) => {
+  const AllGallery = await db
+    .get()
+    .collection(GALLERY_COLLECTION)
+    .find()
+    .toArray();
+  if (AllGallery) {
+    res.status(200).json(AllGallery);
+  } else {
+    res.status(201).json("Gallery Empty");
+  }
+});
+const DeleteGallery = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  const deleteGallery = await db
+    .get()
+    .collection(GALLERY_COLLECTION)
+    .deleteOne({ _id: ObjectId(id) });
+  if (deleteGallery) {
+    res.status(200).json(deleteGallery);
+  } else {
+    res.status(500).json("Something Went Wrong ");
+  }
+});
 
 const AddClients = asyncHandler((req, res) => {
+  const obj = req.body;
 
-  const obj = req.body
-
-  const add = db.get().collection(CLIENTS_COLLECTION).insertOne(obj)
+  const add = db.get().collection(CLIENTS_COLLECTION).insertOne(obj);
   if (add) {
-    res.status(200).json("Success")
+    res.status(200).json("Success");
   } else {
-    res.status(500).json("Somthing Went Wrong")
+    res.status(500).json("Somthing Went Wrong");
   }
-})
+});
 const ViewAllClients = asyncHandler(async (req, res) => {
-  const AllDoctors = await db.get().collection(CLIENTS_COLLECTION).find().toArray()
+  const AllDoctors = await db
+    .get()
+    .collection(CLIENTS_COLLECTION)
+    .find()
+    .toArray();
   if (AllDoctors) {
-    res.status(200).json(AllDoctors)
+    res.status(200).json(AllDoctors);
   } else {
-    res.status(201).json("Gallery Empty")
+    res.status(201).json("Gallery Empty");
   }
-})
-const DeleteClients =asyncHandler(async(req,res)=>{
-  const id=req.params.id
-  const deleteDoctors=await db.get().collection(CLIENTS_COLLECTION).deleteOne({_id:ObjectId(id)})
- if(deleteDoctors){
-  res.status(200).json(deleteDoctors)
- }else{
-  res.status(500).json("Something Went Wrong ")
- }
-})
+});
+const DeleteClients = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  const deleteDoctors = await db
+    .get()
+    .collection(CLIENTS_COLLECTION)
+    .deleteOne({ _id: ObjectId(id) });
+  if (deleteDoctors) {
+    res.status(200).json(deleteDoctors);
+  } else {
+    res.status(500).json("Something Went Wrong ");
+  }
+});
 
-module.exports = { Login, AddGallery,viewAllGallery ,DeleteGallery,AddClients,ViewAllClients,DeleteClients};
+module.exports = {
+  Login,
+  AddGallery,
+  viewAllGallery,
+  DeleteGallery,
+  AddClients,
+  ViewAllClients,
+  DeleteClients,
+};
